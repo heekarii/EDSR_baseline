e33bf57034d9d22b560f4fab8135870c9200bdeb
heekarii <hantecude123@gmail.com>
2025-02-16 01:08:22 +0900

    feat: modify test.py
    - delete match_size function
    - add hr image path
    - add printing time to make sr image

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from EDSR import EDSR  # 모델이 model.py에 저장되어 있다고 가정
 import argparse
 from psnr import calculate_psnr
 from pytorch_msssim import ssim
+import datetime
 
 # 이미지 전처리 함수
 def preprocess_image(image_path):
@@ -23,14 +24,6 @@ def bicubic_downsample(image, scale=4):
     image_np = cv2.resize(image_np, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
     image_tensor = transforms.ToTensor()(image_np).unsqueeze(0)  # 다시 텐서로 변환
     return image_tensor
-
-# 크기 일치시키기 (input_image와 output_image의 해상도를 맞춤)
-def match_size(tensor, reference_tensor):
-    _, _, h_ref, w_ref = reference_tensor.shape
-    image_np = tensor.squeeze(0).cpu().detach().numpy().transpose(1, 2, 0)  # (C, H, W) -> (H, W, C)
-    image_np = cv2.resize(image_np, (w_ref, h_ref), interpolation=cv2.INTER_CUBIC)
-    image_tensor = transforms.ToTensor()(image_np).unsqueeze(0)  # 다시 텐서로 변환
-    return image_tensor.to(reference_tensor.device)
 
 # 결과 저장 함수
 def save_image(tensor, output_path):
@@ -52,6 +45,7 @@ def load_model(model_path, device):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="EDSR Image Super-Resolution")
     parser.add_argument("--model", type=str, required=True, help="Path to trained model (pth file)")
+    parser.add_argument("--hr", type=str, help="Path to hr image")
     parser.add_argument("--input", type=str, required=True, help="Path to input image")
     parser.add_argument("--output", type=str, required=True, help="Path to save output image")
     parser.add_argument("--downsample", action="store_true", help="Apply bicubic downsampling before processing")
@@ -68,19 +62,23 @@ if __name__ == "__main__":
     # Bicubic 다운샘플링 적용 여부 확인
     if args.downsample:
         input_image = bicubic_downsample(input_image).to(device)
-    
+        save_image(input_image, "C:\\Users\\qazwdf11\\Desktop\\ttt\\lr.jpeg")
+
+    start = datetime.datetime.now()
     # 업스케일링 수행
     with torch.no_grad():
         output_image = model(input_image)
-    
+
+    end = datetime.datetime.now()
     # 해상도 맞추기 (PSNR 및 SSIM 계산을 위해)
-    output_image = match_size(output_image, input_image)
+    # output_image = match_size(output_image, input_image)
     
     # PSNR 및 SSIM 계산
-    psnr_value = calculate_psnr(input_image, output_image)
-    ssim_value = ssim(output_image, input_image, data_range=1.0, size_average=True).item()
+    hr_image = preprocess_image(args.hr).to(device)
+    psnr_value = calculate_psnr(hr_image, output_image)
+    ssim_value = ssim(output_image, hr_image, data_range=1.0, size_average=True).item()
     
-    print(f'PSNR: {psnr_value:.2f}, SSIM: {ssim_value:.4f}')
+    print(f'PSNR: {psnr_value:.2f}, SSIM: {ssim_value:.4f}, Time: {end - start}')
     
     # 결과 저장
     save_image(output_image, args.output)
